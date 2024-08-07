e7d9ac36ab511c22c956ce3b37a691305cb26167
Takahito Urai <uraitakahito@gmail.com>
2024-08-07 23:07:14 +0900

    Format main.go

--- src/bankapi/main.go
+++ src/bankapi/main.go
@@ -43,87 +43,87 @@ func main() {
 		},
 	}
 
-    http.HandleFunc("/statement", statement)
-		http.HandleFunc("/deposit", deposit)
-		http.HandleFunc("/withdraw", withdraw)
-		http.HandleFunc("/transfer", transfer)
-    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	http.HandleFunc("/statement", statement)
+	http.HandleFunc("/deposit", deposit)
+	http.HandleFunc("/withdraw", withdraw)
+	http.HandleFunc("/transfer", transfer)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func statement(w http.ResponseWriter, req *http.Request) {
-    numberqs := req.URL.Query().Get("number")
+	numberqs := req.URL.Query().Get("number")
 
-    if numberqs == "" {
-        fmt.Fprintf(w, "Account number is missing!")
-        return
-    }
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
 
-    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
-        fmt.Fprintf(w, "Invalid account number!")
-    } else {
-        account, ok := accounts[number]
-        if !ok {
-            fmt.Fprintf(w, "Account with number %v can't be found!", number)
-        } else {
-						json.NewEncoder(w).Encode(bank.Statement(account))
-        }
-    }
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			json.NewEncoder(w).Encode(bank.Statement(account))
+		}
+	}
 }
 
 func deposit(w http.ResponseWriter, req *http.Request) {
-    numberqs := req.URL.Query().Get("number")
-    amountqs := req.URL.Query().Get("amount")
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
 
-    if numberqs == "" {
-        fmt.Fprintf(w, "Account number is missing!")
-        return
-    }
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
 
-    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
-        fmt.Fprintf(w, "Invalid account number!")
-    } else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
-        fmt.Fprintf(w, "Invalid amount number!")
-    } else {
-        account, ok := accounts[number]
-        if !ok {
-            fmt.Fprintf(w, "Account with number %v can't be found!", number)
-        } else {
-            err := account.Deposit(amount)
-            if err != nil {
-                fmt.Fprintf(w, "%v", err)
-            } else {
-                fmt.Fprintf(w, account.Statement())
-            }
-        }
-    }
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Deposit(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, account.Statement())
+			}
+		}
+	}
 }
 
 func withdraw(w http.ResponseWriter, req *http.Request) {
-    numberqs := req.URL.Query().Get("number")
-    amountqs := req.URL.Query().Get("amount")
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
 
-    if numberqs == "" {
-        fmt.Fprintf(w, "Account number is missing!")
-        return
-    }
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
 
-    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
-        fmt.Fprintf(w, "Invalid account number!")
-    } else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
-        fmt.Fprintf(w, "Invalid amount number!")
-    } else {
-        account, ok := accounts[number]
-        if !ok {
-            fmt.Fprintf(w, "Account with number %v can't be found!", number)
-        } else {
-            err := account.Withdraw(amount)
-            if err != nil {
-                fmt.Fprintf(w, "%v", err)
-            } else {
-                fmt.Fprintf(w, account.Statement())
-            }
-        }
-    }
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Withdraw(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, account.Statement())
+			}
+		}
+	}
 }
 
 func transfer(w http.ResponseWriter, req *http.Request) {
