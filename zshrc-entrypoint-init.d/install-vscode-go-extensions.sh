abd53ee290d23efcf4abc04986283d46416ac87f
Takahito Urai <uraitakahito@gmail.com>
2024-08-07 13:16:27 +0900

    Install GitHub copilot

--- zshrc-entrypoint-init.d/install-vscode-go-extensions.sh
+++ zshrc-entrypoint-init.d/install-vscode-go-extensions.sh
@@ -3,6 +3,8 @@
 # Install Visual Studio Code Go extensions
 #
 extensions=(
+  # https://marketplace.visualstudio.com/items?itemName=GitHub.copilot
+  "GitHub.copilot"
   # https://marketplace.visualstudio.com/items?itemName=golang.Go
   "golang.Go"
 )
